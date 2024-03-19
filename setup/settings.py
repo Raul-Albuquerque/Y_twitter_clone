0407550354ebbd0dab1dd6a047d7357077633a10
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2024-03-18 23:41:33 -0300

    feat: add db config

--- setup/settings.py
+++ setup/settings.py
@@ -65,8 +65,12 @@ WSGI_APPLICATION = "setup.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "db_y",
+        "USER": 'postgres',
+        "PASSWORD": 'kite',
+        "HOST": 'localhost',
+        "PORT": "5432",
     }
 }
 
