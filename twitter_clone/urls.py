e153d0324c5254e7e5f95790d5b9fe21fadf1726
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2024-03-21 02:07:25 -0300

    feat: add login and signup templates

--- twitter_clone/urls.py
+++ twitter_clone/urls.py
@@ -2,5 +2,8 @@ from .views import home_view
 from django.urls import path
 
 urlpatterns = [
-    path("", home_view.home, name="home")
+    path("", home_view.base, name="base"),
+    path("home/", home_view.home, name="home"),
+    path("signup/", home_view.signup, name="signup"),
+    path("signin/", home_view.signin, name="signin")
 ]
--- twitter_clone/views/home_view.py
+++ twitter_clone/views/home_view.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def base(request):
+  return render(request, 'base.html')
+
 def home(request):
   return render(request, 'home.html')
+
+def signup(request):
+  return render(request, 'signup.html')
+
+def signin(request):
+  return render(request, 'signin.html')
