84e94d8d629537f431cc0e8f282968094e52d9cb
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2024-03-28 02:12:26 -0300

    feat: delete a tweet

--- twitter_clone/urls.py
+++ twitter_clone/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import home_view, like_tweet_view, signup_view, signin_view, feed_tweets_view, feed_profile_view, logout_view
+from .views import home_view, like_tweet_view, signup_view, signin_view, feed_tweets_view, feed_profile_view, logout_view, delete_tweet_view
 
 urlpatterns = [
     path("", home_view.home, name="home"),
@@ -12,6 +12,7 @@ urlpatterns = [
     path("profile/", feed_profile_view.feed_profile, name="profile"),
     path("like_tweet/<int:pk>", like_tweet_view.like_tweet, name="like_tweet"),
     path("logout/", logout_view.user_logout, name="logout"),
+    path("delete_tweet/<int:pk>", delete_tweet_view.delete_tweet, name="delete_tweet")
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ twitter_clone/views/delete_tweet_view.py
@@ -0,0 +1,14 @@
+from django.shortcuts import redirect, get_object_or_404
+
+from ..models import Tweet
+
+def delete_tweet(request, pk):
+  if request.user.is_authenticated:
+    tweet = get_object_or_404(Tweet, id=pk)
+
+    if request.user.username == tweet.user.username:
+      tweet.delete()
+
+      return redirect(request.META.get("HTTP_REFERER"))
+
+  return redirect('home')
