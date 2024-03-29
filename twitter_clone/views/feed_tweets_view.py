adba870c1500d786dc2587a0ed5e6c271287f09d
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2024-03-29 20:22:37 -0300

    fix: only the tweet author can delete the tweet

--- twitter_clone/views/feed_tweets_view.py
+++ twitter_clone/views/feed_tweets_view.py
@@ -2,15 +2,19 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate
 from django.http import HttpResponse
 
+from django.contrib.auth.models import User
+
 
 from ..forms import TweetForm
 from ..models import Tweet, Profile
 
 def feed_tweets(request):
   if request.user.is_authenticated:
+    user = User.objects.get(id=request.user.id)
     profile = Profile.objects.get(user_id=request.user.id)
     form = TweetForm(request.POST or None, request.FILES or None)
     profiles = Profile.objects.exclude(user=request.user)
+    following = Profile.objects.get(user_id=request.user.id)
 
     if request.method == "POST":
       if form.is_valid():
@@ -22,6 +26,6 @@ def feed_tweets(request):
       else:
         form = TweetForm()
     tweets = Tweet.objects.all().order_by("-tweet_date")
-    return render(request, 'feed_tweets.html', {"tweets": tweets, "form": form, "profile": profile, "profiles": profiles})
+    return render(request, 'feed_tweets.html', {"tweets": tweets, "form": form, "profile": profile, "profiles": profiles, "user": user, "following": following})
   else:
     return redirect("home")
